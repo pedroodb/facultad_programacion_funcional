3bb3e9a5d484ec6c46e7a88947f657e51eed184c
Pedro <dalbianco.pedro@gmail.com>
2018-11-27 17:31:22 -0300

    Create foldr.hs

---
+++ foldr.hs
@@ -0,0 +1,14 @@
+map' f [] = []
+map' f (x:xs) = (f x):(map' f xs)
+
+filter' f [] = []
+filter' f (x:xs) = if (f x) then (x:(filter' f xs)) else filter' f xs
+
+foldr' :: (b -> a -> a) -> a -> [b] -> a
+
+foldr' f base [] = base
+foldr' f base (x:xs) = f x (foldr' f base xs)
+
+foldMap' f = foldr' (\x foldedList -> f x : foldedList) []
+foldFilter' f = foldr' (\x foldedList -> if f x then x : foldedList else foldedList) []
+
